92ab798cd86a3406b57384641a0e915ded08b04c
Sean <99593501+Seann0425@users.noreply.github.com>
2023-02-27 16:34:47 +0800

    Create d072.cpp

---
+++ D/d072.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,y;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        cin >> y;
+        cout << "Case " << i << ": ";
+        if(y%4==0&&y%100!=0||y%400==0) cout << "a leap year" << endl;
+        else cout << "a normal year" << endl;
+    }
+}
