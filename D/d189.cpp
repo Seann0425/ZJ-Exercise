7b61c11da736a5093c0dd61774723df77d61cb67
Sean <99593501+Seann0425@users.noreply.github.com>
2023-02-27 16:36:23 +0800

    Create d189.cpp

---
+++ D/d189.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int a,sum;    
+		while(cin >> a){        
+			sum = a;        
+			while(a > 1){            
+				sum += a/3;            
+				a = a/3 + a%3;            
+				if(a == 2){                
+						a++;            
+				}        
+			}        
+			cout << sum << endl;    
+		}
+}
