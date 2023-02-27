b90947af6dae2cad82c266a0a5543838ea870838
Sean <99593501+Seann0425@users.noreply.github.com>
2023-02-27 16:33:26 +0800

    Create d066.cpp

---
+++ D/d066.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a,b;
+    cin >> a >> b;
+    if(60*a+b >= 450 && 60*a+b < 1020) cout << "At School";
+    else cout << "Off School";
+}
