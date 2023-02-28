4bed50d83cba4fc9cd699166e1cf095c593b2cf0
Sean <99593501+Seann0425@users.noreply.github.com>
2023-02-28 20:37:11 +0800

    Create a001.cpp

---
+++ A/a001.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string Input;
+    while(cin >> Input)
+    {
+        cout << "hello, " << Input << endl;
+    }
+    return 0;
+}
