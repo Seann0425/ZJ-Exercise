d040917d797f4034604c3cf838e0f2b7d3c83192
Sean <99593501+Seann0425@users.noreply.github.com>
2023-03-06 18:42:31 +0800

    Create a017.cpp

---
+++ A/a017.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int h, int len);
+string eq;
+
+int main(){
+    while(getline(cin, eq)){
+        cout << solve(0, eq.length());
+    }
+    return 0;
+}
+
+int solve(int h, int len){
+
+}
