47b9b06a354eb99fe439fed9e3c0acfcbc06e8ff
Sean <99593501+Seann0425@users.noreply.github.com>
2023-03-13 20:57:38 +0800

    Update a017.cpp

--- A/a017.cpp
+++ A/a017.cpp
@@ -12,5 +12,19 @@ int main(){
 }
 
 int solve(int h, int len){
-
+    int a=0;
+    for(int i=len-1;i>=h;i--){
+        if(eq[i]==")") a++;
+        if(eq[i]=="(") a--;
+        if(eq[i]=="+"&&a==0){
+            return solve(h, i-1)+solve(i+1, len);
+        }
+        if(eq[i]=="-"&&a==0){
+            return solve(h,i-1)-solve(i+1,len);
+        }
+    }
+    for(int i=len-1;i>=h;i--){
+        if(eq[i]==")") a++;
+        if(eq[i]=="(") a--;
+    }
 }
