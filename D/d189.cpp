97f4d8a1d946a3c101704a6da95ce382a5e7f465
Sean <99593501+Seann0425@users.noreply.github.com>
2024-03-02 20:48:21 +0800

    Update d189.cpp

--- D/d189.cpp
+++ D/d189.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 int main(){
     int a,sum;    
-		while(cin >> a){        
-			sum = a;        
-			while(a > 1){            
-				sum += a/3;            
-				a = a/3 + a%3;            
-				if(a == 2){                
-						a++;            
-				}        
-			}        
-			cout << sum << endl;    
-		}
+	while(cin >> a){        
+	    sum = a;        
+	    while(a > 1){            
+		    sum += a/3;            
+		    a = a/3 + a%3;            
+		    if(a == 2){                
+				a++;            
+		    }        
+	    }        
+	    cout << sum << endl;    
+	}
 }
